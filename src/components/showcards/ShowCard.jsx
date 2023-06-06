49a236de3103ab6854479276a3c02ae3b0c27131
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-06-06 16:56:35 +0600

    show card design components

---
+++ src/components/showcards/ShowCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ShowCard = ({data}) => {
+
+  return (
+    <>
+      <div class="card">
+        <img
+          style={{ width: "100%", height: "250px", objectFit: "cover" }}
+          src="https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">{data.name}</h5>
+          <div className="row">
+            <div className="col-md-6">
+              <p>Premwired : {data.premiered}</p>
+            </div>
+            <div className="col-md-6">
+              <p>Rattings : {data.rating.average} Star</p>
+            </div>
+          </div>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-primary">View Details</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ShowCard;
