c37a211b9b2f4f1735380fe003c95d810565a1ac
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-06-06 16:56:49 +0600

    all shows screen design

---
+++ src/features/shows/ShowsDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowsDetails = () => {
+  return (
+    <div>ShowsDetails</div>
+  )
+}
+
+export default ShowsDetails
---
+++ src/shared/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <div className="container">
+    <div className="row">
+        <div className="col">
+            <p>All Right Reserved</p>
+        </div>
+    </div>
+   </div>
+  )
+}
+
+export default Footer
